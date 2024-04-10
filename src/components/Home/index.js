9d7a0ed2a8a43b3a7064fec19e457eb66f27cfe6
Jonathan Yau <jonathan_yau@outlook.com>
2024-04-10 13:24:40 -0400

    added effects such as animatioon and hover

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 
 
@@ -10,10 +10,21 @@ const Home = () => {
     const nameArray = [ 'o', 'n', 'a', 't', 'h', 'a', 'n']
     const jobArray = ['S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
 
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+
     return (
         <div className="container home-page">
             <div className="text-zone"> 
-                <h1> Hi, <br /> I'm
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i,</span>
+                    <br />
+                <span className={`${letterClass} _13`}>I,</span>
+                <span className={`${letterClass} _14`}>'m   </span>
                 <img src={LogoTitle} alt="developer" />
                 <AnimatedLetters letterClass={letterClass}
                     strArray={nameArray}
@@ -21,7 +32,7 @@ const Home = () => {
                 <br />
                 <AnimatedLetters letterClass={letterClass}
                     strArray={jobArray}
-                    idx={15}/>
+                    idx={22}/>
                 </h1>
                 <h2>Fullstack Developer / Motion Designer</h2>
                 <Link to='/contact' className='flat-button'>CONTACT ME</Link>
