dd28664164e264ca73e9950b30d11da35416a071
Jonathan Yau <jonathan_yau@outlook.com>
2024-08-05 15:26:20 -0400

    deleted unused function from GSAP

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -11,31 +11,6 @@ const Logo = () => {
     const solidLogoRef = useRef()
 
 
-    //Draw SVG Plugin
-    useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin)
-
-        gsap.timeline()
-        .to(bgRef.current, {
-            duration: 1,
-            opacity: 1,
-        })
-        .from(outlineLogoRef.current, {
-            drawSVG: 0,
-            duration: 20,
-        })
-
-        gsap.fromTo(
-            solidLogoRef.current, {
-                opacity: 0,
-            }, {
-                opacity: 1,
-                delay: 4,
-                duration: 4,
-            }
-        )
-    }, [])
-
     return (
         <div className='logo-container' ref={bgRef}>
             <img ref={solidLogoRef} className='solid-logo' src={LogoS} alt='S' />
